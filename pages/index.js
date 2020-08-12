d0b774a60c511e4d9e07a2a16f49d2fb530fe29a
Nick Hvattum <nhvattum@gmail.com>
2020-08-12 15:23:42 -0700

    added title that reirects to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Title from '../components/title'
 
 export default function Home() {
 
 
   return(
     <div>
+      <Title />
       <div>
-        <h1>INSPLORE </h1>
-      </div>
-      <div>
+        <button >
+          Maps
+        </button>
         <footer>
           <ul>
             <li>
--- pages/maps/mapboxViewWorld.js
+++ pages/maps/mapboxViewWorld.js
@@ -1,11 +1,14 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Title from '../../components/title'
 
 export default function MapboxViewWorld() {
 
   const router = useRouter()
 
-  return 
-    <span onClick={() => router.push('/')}>Return to home page</span>
-  
+  return (
+    <div>
+      <Title />
+      <span onClick={() => router.push('/')}>Return to home page</span>
+    </div>
+  )
 }
