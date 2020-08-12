77cd83f4f3fbdf9e855de218f005be69899db823
Nick Hvattum <nhvattum@gmail.com>
2020-08-12 16:14:42 -0700

    buttons are working on homepage

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,19 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Title from '../components/title'
+import { useRouter } from 'next/router'
 
 export default function Home() {
-
+  const router = useRouter()
 
   return(
     <div>
       <Title />
       <div>
-        {console.log(process.env.GREETING)}
         <h1>INSPLORE </h1>
+        {console.log(process.env.GREETING)}
+        <button onClick={() => router.push("/destinations")}>Destinations</button>
+        <button onClick={() => router.push("/randomDestination")}>Spin the Globe</button>
       </div>
       <div>
         <footer>
