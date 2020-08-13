b1e37f5a9eb18ce1c9ecc60e044a0bbab07a7a1c
Nick Hvattum <nhvattum@gmail.com>
2020-08-12 21:32:51 -0700

    about to do a pull request

--- pages/destinations.js
+++ pages/destinations.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import TestCities from './maps/components/testCities.js'
+
+
 import Title from '../components/title'
+import Globe from '../components/globe'
 import { useRouter } from 'next/router'
 
-export default function Destinations() {
-    const router = useRouter()
+export default function Home() {
+  const router = useRouter()
 
-    return (
-        <div>
-            <Title />
-            <div>List of Destinations</div>
-            <button onClick={() => router.push("/maps/mapboxViewWorld")}>See More Info</button>
-            <button onClick={() => router.push("/randomDestination")}>Spin Again</button>
-        </div>
-    )
+
+  return(
+    <div className="home">
+      <Head >
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,700&display=swap" rel="stylesheet"/>
+
+      </Head>
+      <Title />
+      
+      <Globe />
+      <div className="homeButtons">
+        <button onClick={() => router.push("/randomDestination")}>Randomize</button>
+        <button onClick={() => router.push("/destinations")}>Destinations</button>
+      </div>
+      <div>
+        <footer>
+          <ul>
+            <li>
+              <Link href="/maps/mapboxViewWorld">
+                <a title="PLACEHOLDER_SELECT_A_CITY">Select a city (PLACEHOLDER)...</a>
+              </Link>
+            </li>
+          </ul>
+        </footer>
+      </div>
+    </div>
+  )
 }
