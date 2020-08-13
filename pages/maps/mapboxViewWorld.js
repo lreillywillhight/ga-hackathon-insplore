fdb1656d8c6cca24848cf9e7cf7b129d62ca351e
smcleod86 <mcleod.scott_86@yahoo.com>
2020-08-13 10:20:05 -0700

    damn git

--- pages/maps/mapboxViewWorld.js
+++ pages/maps/mapboxViewWorld.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 import Title from '../../components/title'
+import MapSidebar from './components/map-sidebar'
 
 const Map0 = dynamic(() => import('./components/map0'), {
   loading: () => <p>Loading...</p>,
@@ -19,7 +20,10 @@ export default function MapboxViewWorld() {
       <Title />
       <span onClick={() => router.push('/')}>Return to home page</span>
       <h1>HELLO MAPBOX</h1>
-      <Map0 />
+      <div className="map-body">
+        <Map0 />
+        <MapSidebar />
+      </div>
       <footer>
         <span onClick={() => router.push('/')}>Return to home page</span>
       </footer>
