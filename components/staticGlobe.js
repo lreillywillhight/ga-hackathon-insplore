e35382ade736b835ac1d74c2cab9a01e458ec1f7
Nick Hvattum <nhvattum@gmail.com>
2020-08-12 22:16:13 -0700

    I havent committed in a while

--- components/staticGlobe.js
+++ components/staticGlobe.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 
 export default function StaticGlobe() {
 
     return (
         <div className="globeDiv">
+            <Link href='/maps/mapboxViewWorld'>
+                <a>
             <img src="GLOBE STATIC PERU.png"></img>
+                </a>
+            </Link>
         </div>
     )
 }
